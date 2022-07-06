b9bd87278a401611fe2745ccce107b58c74a497f
Alexander Fedotov <alexfov2013@yandex.ru>
2022-07-06 17:44:31 +0300

    first

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import {configureStore} from '@reduxjs/toolkit';
+
+const middlewares = [];
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  middlewares.push(createDebugger());
+}
+
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware().concat(middlewares);
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
